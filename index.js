c265fe8e29079e54bbe8b90a0a676356a0521198
tonomolla6 <tonomollag6@gmail.com>
2021-01-14 21:34:17 +0100

    Prueba 6 de muchas

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const port = process.env.PORT || "6869";
 
 app.get("/", (_, res) => {
-    res.status(200).send("WHATABYTE: Food For Devs");
+  res.status(200).send("WHATABYTE: Food For Devs");
 });
 
 app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
+  console.log(`Listening to requests on http://localhost:${port}`);
 });
